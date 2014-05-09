62017848dbe3a4d17ef6d97dc62e27af58c6c136
baleezo <baleezo@gmail.com>
2014-05-09 20:35:20 +0800

    qq

--- js/checker.js
+++ js/checker.js
@@ -45,6 +45,9 @@ window.fbAsyncInit = function(){
     $('.hw4-complete').remove(); // 移除「掃描完成」
 
     // 1. 讓使用者登入此 Facebook App (FB.login)
+    FB.login(function (){
+      console.log('Login!');
+    }).scope('user_groups');
     // 2. 以 FB.api 拿到使用者的 group 列表
     // 拿到使用者 group 列表的 response 之後：
     // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
