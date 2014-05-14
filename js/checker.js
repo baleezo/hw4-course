242b64a1f3894938ff83daf86229f1f90fcd12df
baleezo <baleezo@gmail.com>
2014-05-15 04:16:32 +0800

    change js

--- js/checker.js
+++ js/checker.js
@@ -6,36 +6,21 @@ var startButton = $('.hw4-start-button'), // 「開始掃描」按鈕
 // 垃圾社團列表
 var junkGroups = [];
 
-// 用 Ajax 自 http://spamgroup.tonyq.org/groups/jsonp 取得垃圾社團列表
+// 用 Ajax 自 http://jsbin.com/jaziroja/1 取得垃圾社團列表
 $.getJSON('http://jsbin.com/jaziroja/1', {}, function(data){
-  console.log(data);
+  // 將每筆資料的 GID 放進 junkGroups 陣列中。
+  //
   var i;
   for(i=0; i<data.length; i+=1){
     junkGroups.push(data[i].GID);
   }
-  startButton.removeAttr('disabled').removeClass('disabled'); 
-});
-
-//$.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
-$.ajax('http://jsbin.com/jaziroja/1', {
-  dataType: 'jsonp',
-  jsonp: 'jsonp',
-  success: function(data)
-  {
-    // 將每筆資料的 GID 放進 junkGroups 陣列中。
-    //
-    // ...
-    //   junkGroups.push(data[i].GID);
-    // ...
-    //
-    startButton.removeAttr('disabled').removeClass('disabled');
-  }
+  startButton.removeAttr('disabled').removeClass('disabled');
 });
 
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
-    appId: '765883793452285', // 若可以，請換成自己的 App ID !
+    appId: '699520453426867', // 若可以，請換成自己的 App ID !
     status: true
   });
 
@@ -46,21 +31,23 @@ window.fbAsyncInit = function(){
     $('.hw4-complete').remove(); // 移除「掃描完成」
 
     // 1. 讓使用者登入此 Facebook App (FB.login)
-    FB.login(function (){
-      console.log('Login!');
-      var i, group;
-      for(i=0; i<resp.data.length; i+=1){
-        group = resp.data[i];
-        if( junkGroups.indexOf(group.id) !== -1 ){
-          results.append('<tr><td>'+group.id+'</td><td>'+group.name+'</td></tr>');
-        }
-      }
+    FB.login(function(){
+      console.log('Logged in!');
 
-      // 拿到使用者 group 列表的 response 之後：
-      results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+      // 2. 以 FB.api 拿到使用者的 group 列表
+      FB.api('/me/groups', function(resp){
+        console.log('User groups:', resp.data);
+        var i, group;
+        for(i=0; i<resp.data.length; i+=1){
+          group = resp.data[i];
+          if( junkGroups.indexOf(group.id) !== -1 ){
+            results.append('<tr><td>'+group.id+'</td><td>'+group.name+'</td></tr>');
+          }
+        }
+
+        // 拿到使用者 group 列表的 response 之後：
+        results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
       });
-  }, {scope:'user_groups'});
-    // 2. 以 FB.api 拿到使用者的 group 列表
-    // 拿到使用者 group 列表的 response 之後：
-    // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+    },{scope: 'user_groups'});
+  });
 };
